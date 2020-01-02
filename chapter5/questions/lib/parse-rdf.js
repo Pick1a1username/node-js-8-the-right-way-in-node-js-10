3ec0abe7161fd67da9c2f40c67b54b2defef5a80
Pick1a1username <20301273+Pick1a1username@users.noreply.github.com>
2020-01-02 10:36:39 +0900

    20200102_1

--- chapter5/questions/lib/parse-rdf.js
+++ chapter5/questions/lib/parse-rdf.js
@@ -13,6 +13,8 @@ module.exports = rdf => {
     book.subjects = $('[rdf\\:resource$="/LCSH"]')
         .parent().find('rdf\\:value')
         .toArray().map(elem => $(elem).text());
-    book.lcc = $('[rdf\\:resource$="/LCC"]').parent().find('rdf\\:value').toArray().map(elem => $(elem).text() )[0];
+    book.lcc = $('[rdf\\:resource$="/LCC"]')
+        .parent().find('rdf\\:value').toArray()
+        .map(elem => $(elem).text() )[0];
     return book;
 };
