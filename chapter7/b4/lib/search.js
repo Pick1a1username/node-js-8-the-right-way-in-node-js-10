c01bf1d98e2893d42985296660c13a717d0551e5
Pick1a1username <20301273+Pick1a1username@users.noreply.github.com>
2020-01-04 09:42:29 +0900

    20200104_0

--- chapter7/b4/lib/search.js
+++ chapter7/b4/lib/search.js
@@ -6,6 +6,7 @@
 'use strict';
 
 const request = require('request');
+const rp = require('request-promise');
 
 module.exports = (app, es) => {
     const url = `http://${es.host}:${es.port}/${es.books_index}/book/_search`;
@@ -42,5 +43,47 @@ module.exports = (app, es) => {
 
             res.status(200).json(esResBody.hits.hits.map(({_source}) => _source));
         });
+    });
+
+    /**
+     * Collect suggested terms for a given field based on a given query.
+     * Example: /api/suggest/authors/lipman
+     */
+    app.get('/api/suggest/:field/:query', (req, res) => {
+        const esReqBody = {
+            size: 0,
+            suggest: {
+                suggestions: {
+                    text: req.params.query,
+                    term: {
+                        field: req.params.field,
+                        suggest_mode: 'always',
+                    },
+                }
+            }
+        };
+
+        const options = {url, json: true, body: esReqBody};
+
+        // const promise = new Promise( (resolve, reject) => {
+        //     request.get(options, (err, esRes, esResBody) => {
+        //         if (err) {
+        //             reject( {error: err} );
+        //             return;
+        //         }
+
+        //         if (esRes.statusCode !== 200) {
+        //             reject( {error: esResBody });
+        //             return;
+        //         }
+
+        //         resolve(esResBody);
+        //     });
+        // });
+
+        // promise
+        rp({url, json: true, body: esReqBody})
+        .then(esResBody => res.status(200).json(esResBody.suggest.suggestions))
+        .catch(({error}) => res.tatus(error.status || 502).json(error));
     });
 };
