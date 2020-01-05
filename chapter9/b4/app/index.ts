90f0d09efaefb5ea2cb1c92d7344f0cf4b6575ef
Pick1a1username <20301273+Pick1a1username@users.noreply.github.com>
2020-01-05 16:33:59 +0900

    20200105_5

---
+++ chapter9/b4/app/index.ts
@@ -0,0 +1,148 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import * as templates from './templates.ts';
+import { addBundleForm } from './templates';
+
+// Page setup
+document.body.innerHTML = templates.main();
+const mainElement = document.body.querySelector('.b4-main');
+const alertsElement = document.body.querySelector('.b4-alerts');
+
+// mainElement.innerHTML = templates.welcome();
+// alertsElement.innerHTML = templates.alert({
+//     type: 'info',
+//     message: 'Handlebars is working!',
+// });
+
+/**
+ * Use Window location hash to show the specified view.
+ */
+const showView = async () => {
+    const [view, ...params] = window.location.hash.split('/');
+
+    switch (view) {
+        case '#welcome':
+            mainElement.innerHTML = templates.welcome();
+            break;
+        case '#list-bundles':
+            const bundles = await getBundles();
+            listBundles(bundles);
+            break;
+        default:
+            // Unrecognized view.
+            throw Error(`Unrecognized view: ${view}`);
+    }
+};
+
+window.addEventListener('hashchange', showView);
+
+showView().catch(err => window.location.hash = '#welcome');
+
+const getBundles = async () => {
+    const esRes = await fetch('/es/b4/bundle/_search?size=1000');
+    const esResBody = await esRes.json();
+
+    return esResBody.hits.hits.map(hit => ({
+        id: hit._id,
+        name: hit._source.name,
+    }));
+};
+
+const listBundles = bundles => {
+    mainElement.innerHTML = templates.addBundleForm() + templates.listBundles({bundles});
+
+    const form = mainElement.querySelector('form');
+
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        const name = form.querySelector('input').value;
+        addBundle(name);
+    });
+
+    const deleteButtons = mainElement.querySelectorAll('button.btn-secondary');
+    console.log(deleteButtons);
+
+    for (let i = 0; i < deleteButtons.length; i++) {
+        const deleteButton = deleteButtons[i];
+        deleteButton.addEventListener('click', event => {
+            deleteBundle(deleteButton.getAttribute('data-bundle-id'));
+        });
+    }
+
+};
+
+/**
+ * Show an alert to the user.
+ */
+const showAlert = (message, type = 'danger') => {
+    const html = templates.alert({type, message});
+    alertsElement.insertAdjacentHTML('beforeend', html);
+};
+
+/**
+ * Create a new bundle with the given name, then list bundles.
+ */
+const addBundle = async (name) => {
+    try {
+        // Grab the list of bundles already created.
+        const bundles = await getBundles();
+
+        // Add the new bundle.
+        const url = `/api/bundle?name=${encodeURIComponent(name)}`;
+        const res = await fetch(url, {method: 'POST'});
+        const resBody = await res.json();
+
+        // Merge the new bundle into the original results and show them.
+        bundles.push({id: resBody._id, name});
+        listBundles(bundles);
+
+        showAlert(`Bundle "${name}" created!`, 'success');
+    } catch (err) {
+        showAlert(err);
+    }
+};
+
+
+
+
+/**
+ * Delete the bundle with the specified ID, then list bundles.
+ */
+const deleteBundle = async (bundleId) => {
+    console.log('deleteBundle() triggered!');
+    try {
+        // Delete the bundle, then render the updated list with listBundles().
+
+        // Use getBundles() to retrieve the current list of bundles.
+        const bundles = await getBundles();
+
+        // Find the index of the selected bundleId in the list.
+        // (If there is no matching bundle, throw an exception explaining the problem.)
+        const idx = bundles.findIndex(bundle => bundle.id === bundleId);
+
+        if (idx === -1) {
+            throw {
+                statusCode: 409,
+                error: "The bundle doesn't exist."
+            }
+        }
+
+        // Remove the book from the list.
+        // ???
+
+        // Issue an HTTP DELETE request for the specified bundleId using fetch().
+        const url = `/api/bundle/${bundleId}`;
+        const res = await fetch(url, {method: 'DELETE'});
+        const resBody = await res.json();
+
+        // Remove the bundle from the list by calling splice(), passing in the found index.
+        bundles.splice(idx, 1);
+
+        // Render the updated list using listBundles() and show a success message using showAlert().
+        listBundles(bundles);
+
+        showAlert(`Bundle deleted!`, 'success');
+    } catch (err) {
+        showAlert(err);
+    }
+};
