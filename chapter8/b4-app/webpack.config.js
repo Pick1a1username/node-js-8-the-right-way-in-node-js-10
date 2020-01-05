f51a447b842ba650756756fc39ff831a2ee8a4b1
Pick1a1username <20301273+Pick1a1username@users.noreply.github.com>
2020-01-05 10:43:03 +0900

    20200105_1

--- chapter8/b4-app/webpack.config.js
+++ chapter8/b4-app/webpack.config.js
@@ -3,9 +3,10 @@
 const path = require('path');
 const distDir = path.resolve(__dirname, 'dist');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: './entry.js',
+    entry: './app/index.ts',
     output: {
         filename: 'bundle.js',
         path: distDir,
@@ -18,9 +19,16 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: 'Better Book Bundle Builder',
         }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
     ],
     module: {
         rules: [{
+            test: /\.ts$/,
+            loader: 'ts-loader',
+        },{
             test: /\.css$/,
             use: [ 'style-loader', 'css-loader' ]
         },{
